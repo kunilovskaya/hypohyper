089051d1a8105d8c3d5acf45ddd83332e56e0ec0
akutuzov <akutuzov72@gmail.com>
2020-02-09 19:35:47 +0100

    Correct train to reference

--- input/data/convert_train_to_reference.py
+++ input/data/convert_train_to_reference.py
@@ -7,9 +7,9 @@ def main():
     with open(sys.argv[1], 'r', encoding='utf-8')as f, open(sys.argv[2], 'w', encoding='utf-8') as w:
         _ = next(f)
         for line in f:
-            synset, _, parents, _ = line.split('\t')
+            synset, text, parents, _ = line.split('\t')
             parents = parents.replace("'", '"')
-            w.write(f"{synset}\t{parents}\n")
+            w.write(f"{text}\t{parents}\n")
 
 
 if __name__ == '__main__':
