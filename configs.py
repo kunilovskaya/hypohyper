c498379cf9077bb9afc6605080a8751b491aeec3
u2 <mkunilovskaya@gmail.com>
2020-02-09 04:29:12 +0000

    intrinsic testset:failed quest

--- configs.py
+++ configs.py
@@ -1,9 +1,12 @@
 RANDOM_SEED = 42
-# codename for embeddings: 'ft_araneum', w2v_rdt500, w2v_pos_araneum, ft_ruscorp, ft_araneum_full
-# news_pos_sk, news_pos_sbow, ft_news_sk, ft_news_cbow
-VECTORS = 'w2v_pos_araneum' ## no full means that we filter out OOV at train time
+# codename for embeddings: 'ft-araneum', w2v-rdt500, w2v_pos-araneum, ft-ruscorp, ft-araneum_full
+# news-pos-sk, news-pos-sbow, ft-news-sk, ft-news-cbow
+VECTORS = 'w2v-pos-araneum' ## no full means that we filter out OOV at train time
 POS = 'NOUN' # 'VERB'
 MWE = True
+TEST = 'provided' # intrinsic, provided
+## regularisation strategy: neg-hyp, neg-syn, no-reg
+OPT = 'no-reg'
 
 if 'pos' in VECTORS:
     TAGS = True
@@ -11,11 +14,11 @@ else:
     TAGS = False
     print('Looks like you dont have tags in embeddings?')
     
-if VECTORS == 'w2v_pos_araneum':
+if VECTORS == 'w2v-pos-araneum':
     EMB_PATH = '/home/u2/resources/emb/araneum_upos_skipgram_300_2_2018.bin'
-elif 'ft_araneum' in VECTORS:
+elif 'ft-araneum' in VECTORS:
     EMB_PATH = '/home/u2/resources/emb/araneum_none_fasttextcbow_300_5_2018/araneum_none_fasttextcbow_300_5_2018.model'
-elif 'ft_ruscorp' in VECTORS:
+elif 'ft-ruscorp' in VECTORS:
     EMB_PATH = '/home/u2/resources/emb/181_ruscorpora_fasttext/model.model'
 elif 'rdt' in VECTORS:
     EMB_PATH = '/home/lpvoid/masha/resources/emb/all.norm-sz500-w10-cb0-it3-min5.w2v'
@@ -39,7 +42,7 @@ if 'full' in VECTORS:
 else:
     SKIP_OOV = True
     
-OOV_STRATEGY = 'ft_vector' ##'ft_vector', 'top_hyper', 'vec_on_fly', 'patterns'
+OOV_STRATEGY = 'top-hyper' ##'ft-vector', 'top-hyper'
 MODE = 'single' # if you want to include vectors for main_words in MWE, replace single_wd with main;
 # this this supposed to include vectors for main components of MWE only if this synset has no single_word representation or if MWE is found in vectors
 
