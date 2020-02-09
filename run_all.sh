995c31d8f0c4ecba83cbc97245f4005ab9921d5a
u2 <mkunilovskaya@gmail.com>
2020-02-09 04:28:35 +0000

    intrinsic testset:failed quest

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3.6 format_train.py
+python3.6 get_intrinsic_test.py
+python3.6 format_data.py
 python3.6 learn_projection.py
-# python3.6 test_projection.py
 python3.6 get_hyper_vectors.py
 python3.6 measure_sims.py
 
