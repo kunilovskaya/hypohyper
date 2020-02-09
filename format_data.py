160f91059c6c4ea83c5599e60a80793f5a5f5eec
u2 <mkunilovskaya@gmail.com>
2020-02-09 20:01:04 +0000

    typos

--- format_data.py
+++ format_data.py
@@ -10,10 +10,10 @@ from configs import VECTORS, TAGS, MWE, EMB_PATH, OUT, RUWORDNET, RANDOM_SEED, P
 
 
 parser = argparse.ArgumentParser()
-if POS == 'NOUN':
-    parser.add_argument('--synsets', default='%ssynsets.N.xml' % RUWORDNET, help="synsets files")
-if POS == 'VERB':
-    parser.add_argument('--synsets', default='%ssynsets.V.xml' % RUWORDNET, help="synsets files")
+# if POS == 'NOUN':
+#     parser.add_argument('--synsets', default='%ssynsets.N.xml' % RUWORDNET, help="synsets files")
+# if POS == 'VERB':
+#     parser.add_argument('--synsets', default='%ssynsets.V.xml' % RUWORDNET, help="synsets files")
     
 if TEST == 'intrinsic':
     if POS == 'NOUN':
@@ -28,6 +28,8 @@ if TEST == 'intrinsic':
                             type=os.path.abspath)
 if TEST == 'provided':
     if POS == 'NOUN':
+        # parser.add_argument('--train', default='%strains/static/%s_static-train.tsv' % (OUT, POS), help='static train',
+        #                     type=os.path.abspath)
         parser.add_argument('--train', default='input/data/training_nouns.tsv', type=os.path.abspath)
     if POS == 'VERB':
         parser.add_argument('--train', default='input/data/training_verbs.tsv', type=os.path.abspath)
@@ -37,7 +39,7 @@ start = time.time()
 
 args = parser.parse_args()
 
-parsed_syns = read_xml(args.synsets)
+# parsed_syns = read_xml(args.synsets)
 print('Current embedding model:', EMB_PATH.split('/')[-1], file=sys.stderr)
 model = load_embeddings(EMB_PATH)
 
@@ -46,14 +48,12 @@ os.makedirs(OUT, exist_ok=True)
 
 hypohyper_train = process_tsv(args.train, emb=model, tags=TAGS, mwe=MWE, pos=POS, skip_oov=SKIP_OOV)
 
-print('\n%s train entries: %d\n\n==!!==!!==!!==!!==!!\n' % (TEST.upper(), len(hypohyper_train)), file=sys.stderr)
+print('\n%s train entries: %d\n==!!==!!==!!==!!==!!\n' % (TEST.upper(), len(hypohyper_train)), file=sys.stderr)
 
 ## if any of MWE are in embeddings they look like '::'.join(item.lower().split()) now regardless whether with PoS-tags or without
 ## this outputs the LOWERCASED words, too
 write_hyp_pairs(hypohyper_train, '%s%s_%s_%s_train.tsv.gz' % (OUT, VECTORS, POS, TEST))
-all_pairs = []
-total_wds = 0
-oov_counter = 0
+
 if TEST == 'intrinsic':
     hypohyper_test = process_test_tsv(args.test, emb=model, tags=TAGS, mwe=MWE, pos=POS, skip_oov=SKIP_OOV)
     print('%s test entries: %d' % (TEST.upper(), len(hypohyper_test)), file=sys.stderr)
