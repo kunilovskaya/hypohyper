3348fc01655f37bc25862f988fea6a94440802c5
u2 <mkunilovskaya@gmail.com>
2020-02-10 18:53:32 +0000

    facilities for METHOD == 'deworded' learning

--- learn_projection.py
+++ learn_projection.py
@@ -7,12 +7,12 @@ import pandas as pd
 import sys, os
 import numpy as np
 import time
-from hyper_imports import filter_dataset
-from configs import VECTORS, EMB_PATH, OUT, POS, SKIP_OOV, METHOD, TEST
-
+from hyper_imports import filter_dataset, read_xml, id2name_dict, id2wds_dict, synsets_vectorized
+from configs import VECTORS, EMB_PATH, OUT, POS, SKIP_OOV, METHOD, TEST, RUWORDNET, TAGS
+from collections import defaultdict
 
 parser = ArgumentParser()
-parser.add_argument('--trainfile', default='%strains/%s_%s_%s_train.tsv.gz' % (OUT, VECTORS, POS, TEST),
+parser.add_argument('--trainfile', default='%strains/%s_%s_%s_%s_train.tsv.gz' % (OUT, VECTORS, POS, METHOD, TEST),
                     help="0.8 static train of pre-processed training_data, all UPPER",
                     type=os.path.abspath)
 
@@ -39,29 +39,56 @@ model = load_embeddings(EMB_PATH)
 source_vecs = []
 target_vecs = []
 
-## in the train data, hyponym--hyperonym are not necessarily one2one correspondences
-## multiple hypo-hypernymic relations are typical for polysemantic words,
-# e.g CASE -> example/instance; CASE -> container; CASE -> a piece of furniture; CASE -> a set of facts
-# mult_hypernyms = {}  # Dictionary of hypernyms corresponding to each hyponym which was used for threhold, but not anymore
-
-for hyponym, hypernym in zip(hyponyms, hypernyms):
-    if SKIP_OOV == True:
-        if hyponym in model.vocab or hypernym in model.vocab: # good for static-train
+if METHOD == 'deworded':
+    if POS == 'NOUN':
+        synsets = '%ssynsets.N.xml' % RUWORDNET
+    elif POS == 'VERB':
+        synsets = '%ssynsets.V.xml' % RUWORDNET
+    else:
+        synsets = None
+        print('Not sure which PoS-domain you want from ruWordNet')
+        
+    parsed_syns = read_xml(synsets)
+    synsets_names = id2name_dict(parsed_syns)# a dict of format 144031-N:АУТИЗМ
+    synset_words = id2wds_dict(parsed_syns) # a dict of format 144031-N:[АУТИЗМ, АУТИСТИЧЕСКОЕ МЫШЛЕНИЕ]
+    ## (id,name)
+    identifier_tuple, syn_vectors = synsets_vectorized(emb=model, worded_synsets=synset_words,
+                                                       named_synsets=synsets_names, tags=TAGS, pos=POS)
+    print('Number of vectorised synsets', len(syn_vectors))
+    lookup = defaultdict()
+    for (id,name), vect in zip(identifier_tuple, syn_vectors):
+        lookup[id] = vect
+    
+    for hyponym, hypernym in zip(hyponyms, hypernyms):
+        source_vec = lookup[hyponym]
+        target_vec = lookup[hypernym]
+        source_vecs.append(source_vec)
+        target_vecs.append(target_vec)
+    
+else:
+    ## in the train data, hyponym--hyperonym are not necessarily one2one correspondences
+    ## multiple hypo-hypernymic relations are typical for polysemantic words,
+    # e.g CASE -> example/instance; CASE -> container; CASE -> a piece of furniture; CASE -> a set of facts
+    # mult_hypernyms = {}  # Dictionary of hypernyms corresponding to each hyponym which was used for threhold, but not anymore
+    
+    for hyponym, hypernym in zip(hyponyms, hypernyms):
+        if SKIP_OOV == True:
+            if hyponym in model.vocab or hypernym in model.vocab: # good for static-train
+                source_vec = model[hyponym]
+                target_vec = model[hypernym]
+                source_vecs.append(source_vec)
+                target_vecs.append(target_vec)
+            else:
+                continue
+                
+        elif SKIP_OOV == False:
             source_vec = model[hyponym]
             target_vec = model[hypernym]
             source_vecs.append(source_vec)
             target_vecs.append(target_vec)
         else:
-            continue
-            
-    elif SKIP_OOV == False:
-        source_vec = model[hyponym]
-        target_vec = model[hypernym]
-        source_vecs.append(source_vec)
-        target_vecs.append(target_vec)
-    else:
-        print(hyponym, hypernym, 'not found!', file=sys.stderr)
-
+            print(hyponym, hypernym, 'not found!', file=sys.stderr)
+    
 print('Whole train dataset shape:', len(source_vecs), file=sys.stderr)
 # print('Learning projection matrix...', file=sys.stderr)
 
