85de21c0f35ae5d789e4b9ec43ad1f28ca29441d
u2 <mkunilovskaya@gmail.com>
2020-02-12 16:39:34 +0000

    cleaner

--- get_hyper_vectors.py
+++ get_hyper_vectors.py
@@ -11,41 +11,36 @@ import time
 from configs import VECTORS, TAGS, EMB_PATH, OUT, FT_EMB, OOV_STRATEGY, POS, METHOD, TEST
 
 parser = ArgumentParser()
-# for ultimate results to submit use private instead of public
-if TEST == 'provided':
+# for ultimate results use private instead of public
+if TEST == 'codalab':
     if POS == 'NOUN':
         parser.add_argument('--test', default='input/data/public_test/nouns_public.tsv', type=os.path.abspath)
     if POS == 'VERB':
         parser.add_argument('--test', default='input/data/public_test/verbs_public.tsv', type=os.path.abspath)
-if TEST == 'intrinsic' or TEST == 'random':
-    if POS == 'NOUN':
-        parser.add_argument('--test', default='%strains/%s_%s_%s_test4testing.txt' % (OUT, VECTORS, POS, TEST), type=os.path.abspath)
-    if POS == 'VERB':
-        parser.add_argument('--test', default='%strains/%s_%s_%s_test4testing.txt' % (OUT, VECTORS, POS, TEST), type=os.path.abspath)
-        
-parser.add_argument('--projection', default='%sprojections/%s_%s_%s_%s_projection.npy' % (OUT, VECTORS, POS, METHOD, TEST))
+if TEST == 'static' or TEST == 'random' or TEST == 'provided':
+        parser.add_argument('--test', default='%strains/%s_%s_%s_%s_WORDS.txt' % (OUT, VECTORS, POS, TEST, METHOD), type=os.path.abspath)
+parser.add_argument('--projection', default='%sprojections/%s_%s_%s_%s_projection.npy' % (OUT, VECTORS, POS, TEST, METHOD))
 parser.add_argument('--nr', type=int, default=10, help='Number of candidates')
 
 args = parser.parse_args()
 
 start = time.time()
 
-datafile = args.test
-
+## preprocess words
 if TAGS == True:
     if POS == "NOUN":
-        test_hyponyms = [i.strip().lower()+'_NOUN' for i in open(datafile, 'r').readlines()]
+        test_hyponyms = [i.strip().lower()+'_NOUN' for i in open(args.test, 'r').readlines()]
     elif POS == "VERB":
-        test_hyponyms = [i.strip().lower()+'_VERB' for i in open(datafile, 'r').readlines()]
+        test_hyponyms = [i.strip().lower()+'_VERB' for i in open(args.test, 'r').readlines()]
     else:
         test_hyponyms = None
 else:
-    test_hyponyms = [i.strip().lower() for i in open(datafile, 'r').readlines()]
+    test_hyponyms = [i.strip().lower() for i in open(args.test, 'r').readlines()]
 
 print('Current embedding model:', EMB_PATH.split('/')[-1], file=sys.stderr)
 model = load_embeddings(EMB_PATH)
 
-#### use FT to bust OOV in the input
+## use FT to bust OOV in the input
 if OOV_STRATEGY == 'ft-vector':
     print('FT embedding model:', FT_EMB.split('/')[-1], file=sys.stderr)
     ft_model = load_embeddings(FT_EMB)
@@ -53,7 +48,7 @@ else:
     ft_model = None
 
     
-## TODO other strategies to eliminate OOV
+## TODO other strategies to eliminate OOV: use cooc stats
 #### get a vector of the name of most frequent hypernym synset in the training data
 
 projection = np.load(args.projection)
@@ -89,25 +84,23 @@ for hyponym in test_hyponyms:
 
     hyper_collector.append(predicted_vector)
     
-    if counter % 100 == 0:
+    if counter % 500 == 0:
         print('%d hyponyms processed out of %d total' % (counter, len(test_hyponyms )),
               file=sys.stderr)
         # Want to see predictions in real time?
         print(hyponym, '\t', candidates)
     counter += 1
 
-print('\nOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
-print('====== Number of OOV in test: %d (%d%%)' % (len(oov_in_test), len(oov_in_test)/len(test_hyponyms)*100))
-print('OOV in test\n%s' % oov_in_test)
-print('OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n')
+print('\n====== Number of OOV in test: %d (%d%%)' % (len(oov_in_test), len(oov_in_test)/len(test_hyponyms)*100))
+# print('OOV in test\n%s\n' % oov_in_test)
 
 OUT = '%spredicted_hypers/' % OUT
 os.makedirs(OUT, exist_ok=True)
 
-np.save('%s%s_%s_%s_%s_hypers.npy' % (OUT, VECTORS, POS, METHOD, TEST), hyper_collector)
+np.save('%s%s_%s_%s_%s_hypers.npy' % (OUT, VECTORS, POS, TEST, METHOD), hyper_collector)
 
 end = time.time()
 training_time = int(end - start)
-print('\n%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-print('%s has run.\n Learning hypernym vectors for the real testset: %s minutes' % (os.path.basename(sys.argv[0]), str(round(training_time / 60))))
-print('%%%%%%%%%%%%%%%%%%%%%%%%%%%\n')
+
+print('=== %s has run. ===\nLearning hypernym vectors for the real testset: %s minutes' % (os.path.basename(sys.argv[0]), str(round(training_time / 60))))
+
