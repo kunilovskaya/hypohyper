1e08f6c238ff57e35aaa06b8e2817b8a69d6a736
u2 <mkunilovskaya@gmail.com>
2020-02-18 00:16:17 +0000

    comments

--- run_all.sh
+++ run_all.sh
@@ -3,6 +3,8 @@
 python3.6 format_data.py
 python3.6 learn_projection.py
 python3.6 get_hyper_vectors.py
+# if you want to use corp-info FILTER_1 generate the dictionaries with frequently cooccuring words
+# it is only available for news-pos-cbow, w2v-pos-ruscorpwiki as VECTORS
 python3.6 predict_synsets.py
 python3.6 intrinsic_evaluate.py
 
