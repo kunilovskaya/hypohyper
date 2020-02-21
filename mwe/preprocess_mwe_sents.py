3426bfdf458a7f9a2dac2936c83cd69b13861e11
u2 <mkunilovskaya@gmail.com>
2020-02-21 19:11:59 +0000

    optimized mwe gluing

--- mwe/preprocess_mwe_sents.py
+++ mwe/preprocess_mwe_sents.py
@@ -12,10 +12,10 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from collections import OrderedDict
 from operator import itemgetter
-
+import gzip
 from smart_open import open
 from configs import OUT, POS
-
+import ahocorasick
 
 if __name__ == "__main__":
     OUT_MWE = '%smwe/' % OUT
@@ -27,43 +27,55 @@ if __name__ == "__main__":
 
     start = time.time()
     
-    words = []
+    words = set()
 
     for line in open(args.tagged, 'r').readlines():
         line = line.strip()
         if line in ['ние_NOUN', 'к_NOUN', 'то_PRON', 'тот_DET', 'мочь_VERB', 'чать_VERB', 'нать_VERB', 'аз_NOUN', 'в_NOUN', 'дание_NOUN']:
-            # print(line)
+            continue
+        if len(line.split()) == 1:
             continue
         else:
-            words.append(line)
-    words = set(words)
+            words.add(line)
+            
+    ## optimised iteration and string matching
+    auto = ahocorasick.Automaton()
+    for substr in words:  ## listSubstrings
+        auto.add_word(substr, substr)
+    auto.make_automaton()
+    
     count = 0
     print('%d lempos of words/phrases read' % len(words), file=sys.stderr)
     freq_dict = defaultdict(int)
-    with open('%smwe_vectors_corpus_araneum-rncwiki-news-rncP-pro.gz' % OUT_MWE, 'a') as outfile:
-        for line in sys.stdin: # zcat corpus.txt.gz | python3 this_script.py
-            res = line.strip()
-            count += 1
-            if count % 10000000 == 0:
-                print('%d lines processed, %.2f%% of the araneum only corpus' % (count, count/748880899*100), file=sys.stderr)
-            for i in words:
-                if i in res:
-                    ## glue item
-                    i_dup = i.replace(' ', '::')
-                    res = res.replace(i, i_dup)
-                    ## get a freq_dict: do I have enough to learn vectors for MWE?
-                    freq_dict[i] += 1
-                    ## white to file
-                    outfile.write(res+'\n')
-                    
+    with open('%sBRUTEmwe_vectors_corpus_araneum-rncwiki-news-rncP-pro.gz' % OUT_MWE, 'a') as outfile:
+        # with gzip.open('/home/u2/temp/pro_lempos_ol.gz', 'rb') as f:
+            for line in sys.stdin:# f: # zcat corpus.txt.gz | python3 this_script.py
+                res = line.strip()
+                # res = line.decode("utf-8").strip()
+                count += 1
+                if count % 1000 == 0:
+                    print('%d lines processed, %.2f%% of the araneum only corpus' % (count, count/19030*100), file=sys.stderr) ##748880899
+                seen = set()
+
+                for end_ind, found in auto.iter(res):
+                    print(found) # + res
+                    if found not in seen:
+                        seen.add(found)
+
+                        found_dup = found.replace(' ', ':')
+                        res = res.replace(found, found_dup)
+                        ## get a freq_dict: do I have enough to learn vectors for MWE?
+                        freq_dict[found] += 1
+                        ## white to file
+                        outfile.write(res+'\n')
+
     freq_dict_sort = OrderedDict(sorted(freq_dict.items(), key=itemgetter(1), reverse=True))
     first50pairs_ids = {k: freq_dict_sort[k] for k in list(freq_dict_sort)[:50]}
     print('Test2freq_cooc:', first50pairs_ids, file=sys.stderr)
-    
-    json.dump(freq_dict, open('%sfreq_araneum-rncwiki-news-rncP-pro_ruthes%ss.json' % (OUT_MWE, POS), 'w'))
+
+    json.dump(freq_dict, open('%sBRUTEfreq_araneum-rncwiki-news-rncP-pro_ruthes%ss.json' % (OUT_MWE, POS), 'w'))
     print('Written to: %sfreqs_araneum-rncwiki-news-rncP-pro_ruthes%ss.json' % (OUT_MWE, POS), file=sys.stderr)
-                    # print(line) ## this is fed into another script
-                    # break
+
     end = time.time()
     training_time = int(end - start)
 
